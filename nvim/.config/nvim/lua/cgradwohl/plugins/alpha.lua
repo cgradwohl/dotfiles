95e3aae56a8f0950011d9d5ff03f1b25c6af42a5
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-08-07 21:57:29 -0700

    adds alpha dashboard. simple lsp config.

---
+++ nvim/.config/nvim/lua/cgradwohl/plugins/alpha.lua
@@ -0,0 +1,215 @@
+return {
+    "goolord/alpha-nvim",
+    event = "VimEnter",
+    config = function()
+        local alpha = require("alpha")
+        local dashboard = require("alpha.themes.dashboard")
+
+        -- Set header
+        dashboard.section.header.val = {
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                     ]],
+            [[       ████ ██████           █████      ██                     ]],
+            [[      ███████████             █████                             ]],
+            [[      █████████ ███████████████████ ███   ███████████   ]],
+            [[     █████████  ███    █████████████ █████ ██████████████   ]],
+            [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+            [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+            [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+            [[                                                                       ]],
+        }
+
+        vim.api.nvim_set_hl(0, "AlphaHeader", { fg = "#00FF00" })
+        dashboard.section.header.opts.hl = "AlphaHeader"
+
+        -- Set menu
+        dashboard.section.buttons.val = {
+            dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
+            dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>Neotree reveal<CR>"),
+            dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+            dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+            dashboard.button("SPC wr", "󰁯  > Restore Session For Current Directory", "<cmd>SessionRestore<CR>"),
+            dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
+        }
+
+        -- Send config to alpha
+        alpha.setup(dashboard.opts)
+
+        -- Disable folding on alpha buffer
+        vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
+    end,
+}
+
+-- return {
+--     "goolord/alpha-nvim",
+--     dependencies = {
+--       "nvim-tree/nvim-web-devicons",
+--     },
+
+--     config = function()
+--       local alpha = require("alpha")
+--       local dashboard = require("alpha.themes.startify")
+
+--       dashboard.section.header.val = {
+--         [[__ __    ___  _      _       ___          __  ____     ___   ____  ______  __ __  ____     ___                  ]],
+--         [[|  T  T  /  _]| T    | T     /   \        /  ]|    \   /  _] /    T|      T|  T  T|    \   /  _]                ]],
+--         [[|  l  | /  [_ | |    | |    Y     Y      /  / |  D  ) /  [_ Y  o  ||      ||  |  ||  D  ) /  [_                 ]],
+--         [[|  _  |Y    _]| l___ | l___ |  O  |     /  /  |    / Y    _]|     |l_j  l_j|  |  ||    / Y    _]                ]],
+--         [[|  |  ||   [_ |     T|     T|     |    /   \_ |    \ |   [_ |  _  |  |  |  |  :  ||    \ |   [_      __  __  __ ]],
+--         [[|  |  ||     T|     ||     |l     !    \     ||  .  Y|     T|  |  |  |  |  l     ||  .  Y|     T    |  T|  T|  T]],
+--         [[l__j__jl_____jl_____jl_____j \___/      \____jl__j\_jl_____jl__j__j  l__j   \__,_jl__j\_jl_____j    l__jl__jl__j]],
+--       }
+
+--       alpha.setup(dashboard.opts)
+--     end,
+--   }
+
+
+-- return {
+-- 	"goolord/alpha-nvim",
+-- 	dependencies = {
+-- 		"nvim-tree/nvim-web-devicons",
+-- 		"nvim-lua/plenary.nvim",
+-- 		{ "juansalvatore/git-dashboard-nvim", dependencies = { "nvim-lua/plenary.nvim" } },
+-- 	},
+-- 	config = function()
+-- 		-- Import alpha and dashboard
+-- 		local alpha = require("alpha")
+-- 		local dashboard = require("alpha.themes.dashboard")
+
+-- 		-- Define custom icons
+-- 		local icons = {
+-- 			ui = {
+-- 				file = "",
+-- 				files = "",
+-- 				open_folder = "",
+-- 				config = "",
+-- 				git = "",
+-- 				close = "",
+-- 			},
+-- 		}
+
+-- 		local function whitespace_only(str)
+-- 			return str:match("^%s*$") ~= nil
+-- 		end
+
+-- 		local function pad(n)
+-- 			return { type = "padding", val = n }
+-- 		end
+
+-- 		local function center_header(header)
+-- 			local lines = {}
+-- 			for line in string.gmatch(header, "[^\n]+") do
+-- 				table.insert(lines, line)
+-- 			end
+
+-- 			local width = vim.api.nvim_win_get_width(0) -- Get the width of the current window
+-- 			local padding = math.floor((width - #lines[1]) / 2) -- Calculate padding based on the first line length
+-- 			for i, line in ipairs(lines) do
+-- 				lines[i] = string.rep(" ", padding) .. line
+-- 			end
+-- 			return lines
+-- 		end
+
+-- 		local function format_git_header()
+-- 			local git_dashboard_raw = require("git-dashboard-nvim").setup({})
+-- 			local git_dashboard = {}
+-- 			for _, line in ipairs(git_dashboard_raw) do
+-- 				if not whitespace_only(line) then
+-- 					table.insert(git_dashboard, line)
+-- 				end
+-- 			end
+
+-- 			local git_repo = git_dashboard[1]
+-- 			local git_branch = git_dashboard[#git_dashboard]
+
+-- 			if git_repo == nil and git_branch == nil then
+-- 				return {
+-- 					type = "text",
+-- 					val = "",
+-- 					opts = { position = "center" },
+-- 				}, {}
+-- 			end
+
+-- 			local git_branch_section = {
+-- 				type = "text",
+-- 				val = " " .. git_repo .. ":" .. string.sub(git_branch, 5, #git_branch),
+-- 				opts = { position = "center" },
+-- 			}
+
+-- 			return git_branch_section, { unpack(git_dashboard, 2, #git_dashboard - 1) }
+-- 		end
+
+-- 		-- Define custom header with ASCII art or any custom message
+-- 		local ascii_header = [[
+-- ________   _______   ________  ___      ___ ___  _____ ______
+-- |\   ___  \|\  ___ \ |\   __  \|\  \    /  /|\  \|\   _ \  _   \
+-- \ \  \\ \  \ \   __/|\ \  \|\  \ \  \  /  / | \  \ \  \\\__\ \  \
+--  \ \  \\ \  \ \  \_|/_\ \  \\\  \ \  \/  / / \ \  \ \  \\|__| \  \
+--   \ \  \\ \  \ \  \_|\ \ \  \\\  \ \    / /   \ \  \ \  \    \ \  \
+--    \ \__\\ \__\ \_______\ \_______\ \__/ /     \ \__\ \__\    \ \__\
+--     \|__| \|__|\|_______|\|_______|\|__|/       \|__|\|__|     \|__|
+-- 		]]
+
+-- 		local git_branch_section, commit_history = format_git_header()
+
+-- 		for _, line in ipairs(commit_history) do
+-- 			ascii_header = ascii_header .. "\n" .. string.rep(" ", 3) .. line
+-- 		end
+
+-- 		local custom_header = center_header(ascii_header)
+-- 		local header = { type = "text", val = custom_header }
+
+-- 		local buttons = {
+-- 			type = "group",
+-- 			val = {
+-- 				dashboard.button("e", icons.ui.file .. "  New file", "<cmd>new<CR>"),
+-- 				dashboard.button("o", icons.ui.files .. "  Recent Files", "<cmd>Telescope oldfiles<cr>"),
+-- 				dashboard.button("f", icons.ui.open_folder .. "  Explorer", "<cmd>Oil<cr>"),
+-- 				dashboard.button(
+-- 					"c",
+-- 					icons.ui.config .. "  Neovim config",
+-- 					"<cmd>Oil /home/cafebabe/install/dotfiles/config/nvim<cr>"
+-- 				),
+-- 				dashboard.button("g", icons.ui.git .. "  Open Git", "<cmd>Neogit<CR>"),
+-- 				dashboard.button("l", "󰒲  Lazy", "<cmd>Lazy<cr>"),
+-- 				dashboard.button("q", icons.ui.close .. "  Quit NVIM", ":qa<CR>"),
+-- 			},
+-- 		}
+
+-- 		-- Custom footer showing number of plugins loaded
+-- 		local footer = {
+-- 			type = "text",
+-- 			val = { "⚡" .. require("lazy").stats().loaded .. " plugins loaded." },
+-- 			opts = { position = "center", hl = "Comment" },
+-- 		}
+
+-- 		local bottom_section = {
+-- 			type = "text",
+-- 			val = "hello creature ... what will you create today?",
+-- 			opts = { position = "center" },
+-- 		}
+
+-- 		-- Setting up the alpha layout
+-- 		alpha.setup({
+-- 			layout = {
+-- 				pad(4),
+-- 				header,
+-- 				pad(1),
+-- 				git_branch_section,
+-- 				pad(2),
+-- 				buttons,
+-- 				pad(2),
+-- 				bottom_section,
+-- 				pad(2),
+-- 				footer,
+-- 			},
+-- 			position = "center",
+-- 		})
+-- 	end,
+-- }
