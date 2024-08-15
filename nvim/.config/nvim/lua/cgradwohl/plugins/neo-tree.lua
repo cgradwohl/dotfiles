8398acc5ce0f61fadb82b79783503a0c9843aff8
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-08-14 20:44:53 -0700

    save.

--- nvim/.config/nvim/lua/cgradwohl/plugins/neo-tree.lua
+++ nvim/.config/nvim/lua/cgradwohl/plugins/neo-tree.lua
@@ -1,28 +1,20 @@
--- Neo-tree is a Neovim plugin to browse the file system
--- https://github.com/nvim-neo-tree/neo-tree.nvim
-
 return {
-    'nvim-neo-tree/neo-tree.nvim',
-    version = '*',
-    dependencies = {
-        'nvim-lua/plenary.nvim',
-        'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
-        'MunifTanjim/nui.nvim',
-    },
-    cmd = 'Neotree',
-    keys = {
-        { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
-    },
-    opts = {
-        filesystem = {
-            filtered_items = {
-                visible = true,
-            },
-            window = {
-                mappings = {
-                    ['\\'] = 'close_window',
-                },
-            },
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons',
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
         },
-    },
+      },
+    }
+    vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
+    vim.keymap.set('n', '<leader>bf', ':Neotree buffers reveal float<CR>', {})
+  end,
 }
