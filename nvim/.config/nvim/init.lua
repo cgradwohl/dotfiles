a1b8b7b8ddebd4aa6aecc7c07a5d35bdcc804f0c
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-09-27 13:18:48 -0700

    update astro config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -17,10 +17,11 @@ end
 
 -- LazyVim Setup
 require("lazy").setup({
+  -- Imports base plugin list from here: https://github.com/AstroNvim/AstroNvim/tree/main/lua/astronvim/plugins
   {
     "AstroNvim/AstroNvim",
     version = "^4",                -- Remove version tracking to elect for nighly AstroNvim
-    import = "astronvim.plugins",  -- NOTE: Importing base plugin list from here: https://github.com/AstroNvim/AstroNvim/tree/main/lua/astronvim/plugins
+    import = "astronvim.plugins",
     opts = {                       -- AstroNvim options must be set here with the `import` key
       mapleader = " ",             -- This ensures the leader key must be configured before Lazy is set up
       maplocalleader = ",",        -- This ensures the localleader key must be configured before Lazy is set up
@@ -29,8 +30,10 @@ require("lazy").setup({
       update_notifications = true, -- Enable/disable notification about running `:Lazy update` twice to update pinned plugins
     },
   },
-  { import = "community" }, -- Imports local community.lua file
-  { import = "plugins" },   -- Imports local plugins lua module
+  -- Imports local astrocommunity.lua file
+  { import = "astrocommunity" },
+  -- Imports local plugins lua module
+  { import = "plugins" },
 } --[[@as LazySpec]], {
   -- Configure any other `lazy.nvim` configuration options here
   install = { colorscheme = { "astrotheme", "habamax" } },
