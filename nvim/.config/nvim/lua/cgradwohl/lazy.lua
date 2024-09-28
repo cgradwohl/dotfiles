6f1ec3edff4b6f25c7cd07b947942044b39dc8d1
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-09-28 12:31:43 -0700

    clean up config.

---
+++ nvim/.config/nvim/lua/cgradwohl/lazy.lua
@@ -0,0 +1,44 @@
+local lazypath = vim.env.LAZY or vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+if not (vim.env.LAZY or (vim.uv or vim.loop).fs_stat(lazypath)) then
+    -- stylua: ignore
+    vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable",
+        lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+if not pcall(require, "lazy") then
+    -- stylua: ignore
+    vim.api.nvim_echo(
+        { { ("Unable to load lazy from: %s\n"):format(lazypath), "ErrorMsg" }, { "Press any key to exit...", "MoreMsg" } },
+        true, {})
+    vim.fn.getchar()
+    vim.cmd.quit()
+end
+
+
+require("lazy").setup({
+    spec = {
+        -- import your plugins
+        { import = "cgradwohl.astro" },
+        { import = "cgradwohl.astrocommunity" },
+        { import = "cgradwohl.plugins" },
+    },
+    -- Configure any other settings here. See the documentation for more details.
+    -- colorscheme that will be used when installing plugins.
+    install = { colorscheme = { "catppuccin" } },
+    -- automatically check for plugin updates
+    checker = { enabled = true },
+    ui = { backdrop = 100 },
+    performance = {
+        rtp = {
+            -- disable some rtp plugins, add more to your liking
+            disabled_plugins = {
+                "gzip",
+                "netrwPlugin",
+                "tarPlugin",
+                "tohtml",
+                "zipPlugin",
+            },
+        },
+    }
+})
