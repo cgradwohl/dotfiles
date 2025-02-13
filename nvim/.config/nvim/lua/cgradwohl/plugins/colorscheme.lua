f823c47677f75838a162dbd1d0a9104f2815c19b
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2025-02-13 06:31:37 -0800

    feat: new theme

--- nvim/.config/nvim/lua/cgradwohl/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/cgradwohl/plugins/colorscheme.lua
@@ -13,12 +13,20 @@
 --   }
 
 -- eldritch
+-- return {
+--     "eldritch-theme/eldritch.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         vim.cmd([[colorscheme eldritch]])
+--     end
+-- }
+-- lua/plugins/rose-pine.lua
 return {
-    "eldritch-theme/eldritch.nvim",
-    lazy = false,
-    priority = 1000,
-    opts = {},
-    config = function()
-        vim.cmd([[colorscheme eldritch]])
-    end
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine")
+	end,
 }
