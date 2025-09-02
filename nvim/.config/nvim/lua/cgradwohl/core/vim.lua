7b9a78584647869533cc33302eda8d3f48a9f5da
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2025-09-02 11:22:43 -0700

    feat:

--- nvim/.config/nvim/lua/cgradwohl/core/vim.lua
+++ nvim/.config/nvim/lua/cgradwohl/core/vim.lua
@@ -87,7 +87,7 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- force *.yml.j2 to be treated as plain yaml
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-	pattern = { "*.yml.j2", "*.yaml.j2" },
+	pattern = { "*.yml.j2", "*.yaml.j2", "*.yaml.tpl", "*.yml.tpl" },
 	callback = function(ctx)
 		vim.bo[ctx.buf].filetype = "yaml"
 	end,
